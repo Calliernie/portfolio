7e0780be64421c883bd3a68371ed30722a946b88
Ernest Amuzu <ernestamuzu@ymail.com>
2021-03-18 12:20:43 +0000

    init

---
+++ Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero"></div>
+</template>
+
+<script>
+export default {
+  name: Home,
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.hero {
+  background-image: url("/assets/background.png");
+}
+</style>
