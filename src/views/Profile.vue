1639d15c23a7063bafab75cf88ba17ab3fcd5c8a
Ernest Amuzu <ernestamuzu@ymail.com>
2021-05-14 16:30:14 +0000

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -16,7 +16,7 @@
       </v-overlay>
     </v-parallax>
 
-    <v-container>
+    <v-container class="hidden-xs-only">
       <v-row class="pa-15 pt-8 pb-8">
         <v-col cols="2" xs="2" sm="2" md="2">
           <v-card color="transparent" class="pa-2">
@@ -213,6 +213,271 @@
         </v-col>
 
         <v-divider vertical class="my-6"></v-divider>
+
+        <v-col cols="5" xs="5" sm="5" md="5">
+          <v-row class="pl-8">
+            <h3 style="text-decoration: underline">Skills</h3>
+          </v-row>
+          <v-row class="pl-5">
+            <v-col cols="12">
+              <p>Excellent written communication skills</p>
+              <p>Swift Decision Making</p>
+              <p>Self-motivation</p>
+              <p>Teamwork</p>
+              <p>Creativity</p>
+              <p>Ability to work under pressure</p>
+              <p>Great presentation skills</p>
+            </v-col>
+          </v-row>
+
+          <v-row class="pl-8">
+            <h3 style="text-decoration: underline">Languages</h3>
+          </v-row>
+          <v-row class="pl-5">
+            <v-col cols="12">
+              <p>Java (Intermediate)</p>
+              <p>SQL (Intermediate)</p>
+              <p>HTML (Proficient)</p>
+              <p>Javascript (Beginner)</p>
+              <p>CSS (Proficient)</p>
+              <p>PHP (Intermediate)</p>
+              <p>JUnit (Beginner)</p>
+              <p>Selenium (Beginner)</p>
+            </v-col>
+          </v-row>
+
+          <v-row class="pl-8">
+            <h3 style="text-decoration: underline">Design Softwares</h3>
+          </v-row>
+          <v-row class="pl-5">
+            <v-col cols="12">
+              <p>Photoshop (Proficient)</p>
+              <p>Rhinoceros 3D (Proficient)</p>
+              <p>After Effects (Intermediate)</p>
+              <p>Keyshot (Proficient)</p>
+              <p>Adobe XD (Proficient)</p>
+            </v-col>
+          </v-row>
+
+          <v-row class="pl-8">
+            <h3 style="text-decoration: underline">Interests</h3>
+          </v-row>
+          <v-row class="pl-5">
+            <v-col cols="12">
+              <p>Reading</p>
+              <p>Digital Painting</p>
+              <p>Listening to music</p>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container class="hidden-sm-and-up">
+      <v-row class="px-4">
+        <v-col cols="12" xs="12" sm="2" md="2">
+          <v-card class="mx-auto" max-width="">
+            <v-img
+              height="300"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" xs="12" sm="10" md="10" class="px-6">
+          <v-row class="pt-1"> <h3>ERNEST KWAME AMUZU</h3> </v-row>
+          <v-row class="pt-1"> <h3>QA ENGINEER</h3></v-row>
+          <v-row class="pt-1"
+            >Self-motivated Quality Assurance Engineer. I have a lot of
+            experience in developing test cases and testing and verification of
+            software. I aspire to be one of the top Software Engineers globally
+            in the next five (5) years.</v-row
+          >
+        </v-col>
+      </v-row>
+
+      <v-row class="px-7 pt-4">
+        <v-col cols="12" xs="12" sm="7" md="7">
+          <v-row> <h3 style="text-decoration: underline">Education</h3> </v-row>
+          <v-row>
+            <h4>Graduate Trainee (Software Testing and Verification)</h4>
+          </v-row>
+          <v-row><p>AmaliTech Training Academy</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2019 - 2020</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Diploma in Information Technology (Software Engineering)</h4>
+          </v-row>
+          <v-row><p>AmaliTech Training Academy</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2017 - 2019</i></p></v-row
+          >
+
+          <v-row>
+            <h4>B.A Integrated Rural Art and Industry</h4>
+          </v-row>
+          <v-row
+            ><p>Kwame Nkrumah University of Science and Technology</p>
+          </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2011 - 2015</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Certificate in Computer Literacy (Graphic Design)</h4>
+          </v-row>
+          <v-row><p>Imperial College of Professional Studies</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2011</i></p></v-row
+          >
+
+          <v-row>
+            <h4>West African Senior School Certificate</h4>
+          </v-row>
+          <v-row><p>KNUST Senior High School</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2007 - 2011</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Certificate in Computer Literacy</h4>
+          </v-row>
+          <v-row
+            ><p>Kumasi Virtual Centre for Information Technology, KNUST</p>
+          </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2007</i></p></v-row
+          >
+
+          <v-row>
+            <h3 style="text-decoration: underline">Work Experience</h3>
+          </v-row>
+          <v-row>
+            <h4>Dr. T-bone Services</h4>
+          </v-row>
+          <v-row
+            ><p>
+              Assistant craftsman in Welding and Fabrication processes
+              (Industrial Attachment)
+            </p>
+          </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2014</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Tsito Senior High Technical School</h4>
+          </v-row>
+          <v-row><p>Assistant Storekeeper (National Service)</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2015 - 2016</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Ghana Statistical Service</h4>
+          </v-row>
+          <v-row><p>Lister (Multiple Indicator Cluster Survey)</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2017</i></p></v-row
+          >
+
+          <v-row>
+            <h4>
+              Centre for Scientific and Industrial Research (Wood Industry and
+              Utilization Division)
+            </h4>
+          </v-row>
+          <v-row><p>Assistant (EPA BAMBOO TRANSPLANTING PROJECT)</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2016-2017</i></p></v-row
+          >
+
+          <v-row>
+            <h4>Ghana Statistical Service</h4>
+          </v-row>
+          <v-row><p>Enumerator (Agric Census)</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2018</i></p></v-row
+          >
+
+          <v-row>
+            <h4>National Board for Small Scale Industries (NABCO)</h4>
+          </v-row>
+          <v-row><p>Administrative Staff</p> </v-row>
+          <v-row class="pt-0 mt-0"
+            ><p><i>2018 - 2019</i></p></v-row
+          >
+
+          <v-row>
+            <h3 style="text-decoration: underline">Personal Information</h3>
+          </v-row>
+          <v-row class="pr-3">
+            <p>Location</p>
+            <v-spacer></v-spacer>
+            <p>Takoradi, Ghana</p>
+          </v-row>
+
+          <v-row class="pr-3">
+            <p>Email</p>
+            <v-spacer></v-spacer>
+            <p>ernestamuzu@ymail.com</p>
+          </v-row>
+
+          <v-row class="pr-3">
+            <p>LinkedIn</p>
+            <v-spacer></v-spacer>
+            <p>
+              <a href="www.linkedin.com/in/ernest-amuzu">
+                www.linkedin.com/in/ernest-amuzu
+              </a>
+            </p>
+          </v-row>
+
+          <v-row class="pr-3">
+            <p>Facebook</p>
+            <v-spacer></v-spacer>
+            <p>
+              <a href="facebook.com/ernest.amuzu">
+                www.linkedin.com/in/ernest-amuzu
+              </a>
+            </p>
+          </v-row>
+
+          <v-row>
+            <v-col cols="6"><p>Facebook</p></v-col>
+            <v-col cols="6" class="float-right"
+              ><p>
+                <a href="facebook.com/ernest.amuzu"
+                  >facebook.com/ernest.amuzu</a
+                >
+              </p></v-col
+            >
+          </v-row>
+
+          <v-row>
+            <v-col cols="6"><p>LinkedIn</p></v-col>
+            <v-col cols="6" class="float-right"
+              ><p>
+                <a href="www.linkedin.com/in/ernest-amuzu">
+                  www.linkedin.com/in/ernest-amuzu
+                </a>
+              </p></v-col
+            >
+          </v-row>
+
+          <v-row>
+            <v-col cols="6"><p>GitHub</p></v-col>
+            <v-col cols="6" class="float-right"
+              ><p>
+                <a href="www.linkedin.com/in/ernest-amuzu">
+                  www.github.com/calliernie</a
+                >
+              </p></v-col
+            >
+          </v-row>
+        </v-col>
 
         <v-col cols="5" xs="5" sm="5" md="5">
           <v-row class="pl-8">
