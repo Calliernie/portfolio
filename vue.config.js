58e674a74512f17ef02b784f38347046783bc3b9
Ernest Kwame Amuzu <57227547+Calliernie@users.noreply.github.com>
2023-09-16 21:43:58 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  productionSourceMap: false,
-    configureWebpack: {
-        performance: {
-            hints: process.env.NODE_ENV === 'production' ? 'warning' : false
-        }
-    },
   transpileDependencies: ["vuetify"],
 };
